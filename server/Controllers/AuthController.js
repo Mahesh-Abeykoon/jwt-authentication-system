37c553c661526791f87de69dcbb474807dd960ac
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-09-18 22:55:21 +0530

    Remove comments & add response status

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -28,27 +28,20 @@ module.exports.Login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
-    // Check if both email and password are provided
     if (!email || !password) {
       return res.json({ message: 'All fields are required' });
     }
 
-    // Find a user with the provided email
     const user = await User.findOne({ email });
-
-    // If no user is found, send an error response
     if (!user) {
       return res.json({ message: 'Incorrect password or email' });
     }
 
     // Compare the provided password with the stored hashed password
     const auth = await bcrypt.compare(password, user.password);
-
-    // If the passwords don't match, send an error response
     if (!auth) {
       return res.json({ message: 'Incorrect password or email' });
     }
-
     // If the passwords match, generate a secret token for the user
     const token = createSecretToken(user._id);
 
@@ -57,15 +50,11 @@ module.exports.Login = async (req, res, next) => {
       withCredentials: true,
       httpOnly: false,
     });
-
-    // Send a success response indicating that the user is logged in
     res.status(201).json({ message: "User logged in successfully", success: true });
-
-    // Move on to the next middleware or route handler
     next();
   } catch (error) {
-    // Handle any errors that may occur during this process
     console.error(error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
